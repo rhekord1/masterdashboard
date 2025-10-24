081714e17df8a7c59f7af5e8464bbc64778cb10e
rhekord <rhekord1@gmail.com>
2025-10-24 14:12:12 +0000

    Modified app/page.tsx -Added TodoList component to main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { AppBarChart } from "@/components/AppBarChart";
 import { AppAreaChart } from "@/components/AppAreaChart";
 import AppPieChart from "@/components/AppPieChart";
 import CardList from "@/components/CardList";
+import TodoList from "@/components/TodoList";
 
 
 const page = () => {
@@ -10,7 +11,7 @@ const page = () => {
       <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2"><AppBarChart/></div>
       <div className="bg-primary-foreground p-4 rounded-lg "><CardList title="Latest Transactions"/></div>
       <div className="bg-primary-foreground p-4 rounded-lg"><AppPieChart/></div>
-      <div className="bg-primary-foreground p-4 rounded-lg">Test</div>
+      <div className="bg-primary-foreground p-4 rounded-lg"><TodoList/></div>
       <div className="bg-primary-foreground p-4 rounded-lg  lg:col-span-2  xl:col-span-1 2xl:col-span-2"><AppAreaChart/></div>
       <div className="bg-primary-foreground p-4 rounded-lg"><CardList title="Popular Content"/></div>
       
