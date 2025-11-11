6c5f82d618fd8dd6b11e9bfbc216311518b90069
rhekord <rhekord1@gmail.com>
2025-11-11 12:15:03 +0000

    Created EditUser.tsx -added a sheet component from shadcn for editing user details

---
+++ components/EditUser.tsx
@@ -0,0 +1,153 @@
+"use client"
+
+import { useForm } from "react-hook-form";
+import { SheetContent, SheetHeader, SheetTitle, SheetDescription } from "./ui/sheet"
+
+
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "./ui/form";
+import { Input } from "./ui/input";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Button } from "./ui/button";
+
+const formSchema = z.object({
+  username: z
+  .string()
+  .min(2,{message:"Username must be at least 2 characters!"})
+  .max(50),
+    email: z.email({message:"Invalid email address!"}),
+    phone: z.string().min(10).max(15),
+    location: z.string().min(2),
+    role: z.enum(["admin","user"]),
+});
+
+
+
+const EditUser = () => {
+
+     const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "Ben Dork",
+      email: "gokubediako@gmail.com",
+      phone: "+233 203487678",
+      location: "Konoha Ahenkro",
+      role: "admin",
+    },
+  });
+
+
+  return (
+     <SheetContent>
+                    <SheetHeader>
+                      <SheetTitle className="mb-4">Edit User</SheetTitle>
+                      <SheetDescription asChild>
+                       <Form  {...form}>
+                        <form className="space-y-8">
+                        <FormField 
+                        control={form.control} 
+                        name="username" 
+                        render={({field})=>(
+                            <FormItem>
+                             <FormLabel>Username</FormLabel>
+                            <FormControl>
+                            <Input {...field} />
+                            </FormControl>
+                            <FormDescription>
+                             This is your public user name.
+                            </FormDescription>
+                            <FormMessage  />
+                            </FormItem>
+                        )}/>
+
+                        <FormField 
+                        control={form.control} 
+                        name="email" 
+                        render={({field})=>(
+                            <FormItem>
+                             <FormLabel>Email</FormLabel>
+                            <FormControl>
+                            <Input {...field} />
+                            </FormControl>
+                            <FormDescription>
+                            Only admin can see your email.
+                            </FormDescription>
+                            <FormMessage  />
+                            </FormItem>
+                        )}/>
+
+                        <FormField 
+                        control={form.control} 
+                        name="phone" 
+                        render={({field})=>(
+                            <FormItem>
+                             <FormLabel>Phone</FormLabel>
+                            <FormControl>
+                            <Input {...field} />
+                            </FormControl>
+                            <FormDescription>
+                             This is your public user phone number.
+                            </FormDescription>
+                            <FormMessage  />
+                            </FormItem>
+                        )}/>
+
+                        <FormField 
+                        control={form.control} 
+                        name="location" 
+                        render={({field})=>(
+                            <FormItem>
+                             <FormLabel>Location</FormLabel>
+                            <FormControl>
+                            <Input {...field} />
+                            </FormControl>
+                            <FormDescription>
+                             This is your public location.
+                            </FormDescription>
+                            <FormMessage  />
+                            </FormItem>
+                        )}/>
+
+                        <FormField 
+                        control={form.control} 
+                        name="role" 
+                        render={({field})=>(
+                            <FormItem>
+                             <FormLabel>Role</FormLabel>
+                            <FormControl>
+                            <Select>
+                                <SelectTrigger >
+                                    <SelectValue placeholder="Role" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    <SelectItem value="admin">Admin</SelectItem>
+                                    <SelectItem value="user">User</SelectItem>
+                                   
+                                </SelectContent>
+                            </Select>
+                            </FormControl>
+                            <FormDescription>
+                             Only verified users can be admin.
+                            </FormDescription>
+                            <FormMessage  />
+                            </FormItem>
+                        )}/>
+                            <Button type="submit">Submit</Button>
+
+                        </form>
+                       </Form>
+                      </SheetDescription>
+                    </SheetHeader>
+        </SheetContent>
+  )
+}
+
+export default EditUser;
